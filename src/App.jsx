9766e5c47c616690b830fa0f2ffc1b4ed3f4c573
farwara <farwarafiq451@gmail.com>
2026-01-01 21:01:16 +0100

    feat:add protected route for authenticated pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,27 @@ import Layout from './components/Layout/Layout';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
-
+import ProtectedRoute from './routes/ProtectedRoute';
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route element={<Layout />}>
-                <Route path = "/" element={<Login />} />
-                <Route path= "/register" element={<Register />} />
-                <Route path= "/dashboard" element={<Dashboard />} />
-                    </Route>
+                    <Route path="/" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route
+                        path="/dashboard"
+                        element={
+                            <ProtectedRoute>
+                                <Dashboard />
+                            </ProtectedRoute>
+                        }
+                    />
+                </Route>
             </Routes>
         </BrowserRouter>
-);
+    );
 }
 
 export default App;
